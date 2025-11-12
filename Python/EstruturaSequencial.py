a5404240ca34f7dd8974d004cb78ab08d002b38e
prupf <pedroviana@tutamail.com>
2025-11-12 09:21:08 -0300

    Notebook01 e Lista01 em Python

---
+++ Python/EstruturaSequencial.py
@@ -0,0 +1,41 @@
+###########################################
+##  EXERCICIOS - ESTRUTURAS SEQUENCIAIS  ##
+###########################################
+
+
+# 01
+
+#largura = float(input("Digite a largura do terreno: "))
+#comprimento = float(input("Digite o comprimento do terreno: "))
+#valMtQuad = float(input("Digite o valor do metro quadrado: "))
+
+#area = largura * comprimento
+#valFinal = area * valMtQuad
+
+#print(f"Area do terreno: {area:.2f}\r\nPreco do terreno: {valFinal:.2f}")
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+
+# 02
+
+#import math as m
+
+#base = float(input("Informe a base do retângulo: "))
+#altura = float(input("Informe a altura do retângulo: "))
+
+#area = base * altura
+#perimetro = (base * 2) + (altura * 2)
+#diagonal = m.sqrt(m.pow(base, 2) + m.pow(altura, 2))
+
+#print(f"Area: {area:.4f}\r\nPerimetro: {perimetro:.4f}\r\nDiagonal: {diagonal:.4f}")
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+
+# 03
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------
